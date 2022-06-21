290cf69c39de802f32f885b0f9f91c131e86cb1a
Gabriel Gregório <gabrielgregorio@pop-os.localdomain>
2022-06-21 12:03:15 -0300

    chore: altera o path da função fs para atender aos requisitos dos testes

--- routes/talker.js
+++ routes/talker.js
@@ -2,17 +2,15 @@ const router = require('express').Router();
 
 const fs = require('../helpers/fs');
 
-const path = '/home/gabrielgregorio/Projetos/sd-019-c-project-talker-manager/talker.json';
-
 router.get('/', async (_req, res) => {
-  const talker = await fs.read(path);
+  const talker = await fs.read('talker.json');
   // if (!talker) return [];
   return res.status(200).json(talker);
 });
 
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
-  const talker = await fs.read(path);
+  const talker = await fs.read('talker.json');
   const foundTalkerById = talker.find((e) => e.id === Number(id));
   if 
   (!foundTalkerById) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
